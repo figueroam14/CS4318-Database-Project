4445518dad05c53fa906f3915acd8e67f389ba3a
=Michael Figueroa <Michaelfigueroa24@gmail.com>
2020-11-25 18:24:25 -0600

    added create database command

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `hospital_database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hospital_database`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hospital_database
@@ -284,4 +286,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-25 18:21:50
+-- Dump completed on 2020-11-25 18:23:58
